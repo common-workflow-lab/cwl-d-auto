ce2affbb45b4431db5bbb0e9b946693b8e645d7f
Tomoya Tanjo <ttanjo@gmail.com>
2022-12-11 03:11:11 +0000

    Fix update-parsers.sh

--- update-parsers.sh
+++ update-parsers.sh
@@ -2,16 +2,16 @@
 
 set -eu
 
-mkdir -p source/cwl_d_auto
+mkdir -p source/cwl
 
-schema-salad-tool --codegen dlang --codegen-package cwl_d_auto.v1_0 --codegen-parser-info "CWL v1.0 parser generated with schema-salad-tool" \
+schema-salad-tool --codegen dlang --codegen-package cwl.v1_0 --codegen-parser-info "CWL v1.0 parser generated with schema-salad-tool" \
     --codegen-examples resources/cwl-v1.0 \
-    https://github.com/common-workflow-language/common-workflow-language/raw/codegen/v1.0/CommonWorkflowLanguage.yml > source/cwl_d_auto/v1_0.d
+    https://github.com/common-workflow-language/common-workflow-language/raw/codegen/v1.0/CommonWorkflowLanguage.yml > source/cwl/v1_0.d
 
-schema-salad-tool --codegen dlang --codegen-package cwl_d_auto.v1_1 --codegen-parser-info "CWL v1.1 parser generated with schema-salad-tool" \
+schema-salad-tool --codegen dlang --codegen-package cwl.v1_1 --codegen-parser-info "CWL v1.1 parser generated with schema-salad-tool" \
     --codegen-examples resources/cwl-v1.1 \
-    https://github.com/common-workflow-language/cwl-v1.1/raw/codegen/CommonWorkflowLanguage.yml > source/cwl_d_auto/v1_1.d
+    https://github.com/common-workflow-language/cwl-v1.1/raw/codegen/CommonWorkflowLanguage.yml > source/cwl/v1_1.d
 
-schema-salad-tool --codegen dlang --codegen-package cwl_d_auto.v1_2 --codegen-parser-info "CWL v1.2 parser generated with schema-salad-tool" \
+schema-salad-tool --codegen dlang --codegen-package cwl.v1_2 --codegen-parser-info "CWL v1.2 parser generated with schema-salad-tool" \
     --codegen-examples resources/cwl-v1.2 \
-    https://github.com/common-workflow-language/cwl-v1.2/raw/1.2.1_proposed/CommonWorkflowLanguage.yml > source/cwl_d_auto/v1_2.d
+    https://github.com/common-workflow-language/cwl-v1.2/raw/1.2.1_proposed/CommonWorkflowLanguage.yml > source/cwl/v1_2.d

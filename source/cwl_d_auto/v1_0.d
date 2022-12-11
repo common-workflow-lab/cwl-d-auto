992816a7e0e71d45b5dd0859c2da14ee99cc676e
Tomoya Tanjo <ttanjo@gmail.com>
2022-12-11 03:02:19 +0000

    Re-add `cwl_d_auto` module for migration

---
+++ source/cwl_d_auto/v1_0.d
@@ -0,0 +1,9 @@
+/**
+ * Authors: Tomoya Tanjo
+ * Copyright: © 2022 Tomoya Tanjo
+ * License: Apache-2.0
+ */
+deprecated("Use cwl.v1_0 instead.")
+module cwl_d_auto.v1_0;
+
+public import cwl.v1_0;
---
+++ source/cwl_d_auto/v1_2.d
@@ -0,0 +1,9 @@
+/**
+ * Authors: Tomoya Tanjo
+ * Copyright: © 2022 Tomoya Tanjo
+ * License: Apache-2.0
+ */
+deprecated("Use cwl.v1_2 instead.")
+module cwl_d_auto.v1_2;
+
+public import cwl.v1_2;

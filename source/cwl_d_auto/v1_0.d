d09e28162c065812f2ef39040bdd425027fdf64b
Tomoya Tanjo <ttanjo@gmail.com>
2024-09-26 15:36:18 +0900

    Remove deprecated `cwl_d_auto` module

--- source/cwl_d_auto/v1_0.d
+++
@@ -1,9 +0,0 @@
-/**
- * Authors: Tomoya Tanjo
- * Copyright: © 2022 Tomoya Tanjo
- * License: Apache-2.0
- */
-deprecated("Use cwl.v1_0 instead.")
-module cwl_d_auto.v1_0;
-
-public import cwl.v1_0;
--- source/cwl_d_auto/v1_1.d
+++
@@ -1,9 +0,0 @@
-/**
- * Authors: Tomoya Tanjo
- * Copyright: © 2022 Tomoya Tanjo
- * License: Apache-2.0
- */
-deprecated("Use cwl.v1_1 instead.")
-module cwl_d_auto.v1_1;
-
-public import cwl.v1_1;
